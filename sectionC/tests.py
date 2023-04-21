15d62e664f58fa3644416a9af1762d0429f6fc6a
humphrey <hbutau35@gmail.com>
2023-04-21 07:48:08 +0200

    add withX tests

--- sectionC/tests.py
+++ sectionC/tests.py
@@ -19,7 +19,10 @@ def test_validate_isbn_13_number(test_input, expected):
 
 @pytest.mark.parametrize(
     "test_input, expected",
-    [("0316066524", "9780316066525"), ("0-19-852663-6", "9780198526636")],
+    [
+        ("0316066524", "9780316066525"),
+        ("0-19-852663-6", "9780198526636"),
+    ],
 )
 def test_validate_isbn10_number_without_x(test_input, expected):
     assert validate_isbn_number(test_input) == expected
@@ -27,7 +30,7 @@ def test_validate_isbn10_number_without_x(test_input, expected):
 
 @pytest.mark.parametrize(
     "test_input, expected",
-    [("0316066524", "9780316066525")],
+    [("031606652x", "Invalid")],
 )
 def test_validate_isbn10_number_with_x(test_input, expected):
     assert validate_isbn_number(test_input) == expected
